bebc6f11c201da492f19074bd680ccc3622afaa7
Jakub Müller <jakub.muller.so@gmail.com>
2021-03-23 17:24:10 +0100

    user info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function show($id) {
 
+        $user = User::with([
+            'questions',
+            'answers'
+        ])
+        ->where('id', $id)
+        ->first();
+
+        if(!$user) {
+            return response()->json(['message' => 'User with id ' . $id . ' does not exist.'], 404);
+        }
+
+        return response()->json($user, 200);
     }
 
     public function indexQuestions($id) {
